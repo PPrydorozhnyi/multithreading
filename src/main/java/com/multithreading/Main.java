bc62f77cb06302d8d987041886322979533ef503
PPrydorozhnyi <Peter35128@gmail.com>
2020-07-29 14:23:11 +0300

    basic thread stuff

---
+++ src/main/java/com/multithreading/Main.java
@@ -0,0 +1,32 @@
+package com.multithreading;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    log.info("In thread {}. Priority {}", Thread.currentThread().getName(),
+                            Thread.currentThread().getPriority());
+                    throw new IllegalCallerException("Excepty");
+                }
+        );
+
+        thread.setName("Namerino");
+
+        //setting programmatically priority for OS and epoch
+        thread.setPriority(Thread.MAX_PRIORITY);
+
+        thread.setUncaughtExceptionHandler((t, e) -> log.error("Gotcha", e));
+
+        log.info("Currently in thread " + Thread.currentThread().getName() + " before start");
+        thread.start();
+        log.info("Currently in thread " + Thread.currentThread().getName() + " after start");
+
+        Thread.sleep(10_000);
+
+    }
+
+}
